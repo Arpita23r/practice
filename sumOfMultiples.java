b33ffa55a313730014cd6e6c9416934aa4c6e635
Arpita Roy <100989922+Arpita23r@users.noreply.github.com>
2024-09-19 10:17:06 +0530

    Create sumOfMultiples.java

---
+++ sumOfMultiples.java
@@ -0,0 +1,11 @@
+public class Solution{
+static int sumOfMultiple(int n) {
+int sum=0;
+for(int i=0;i<=n;i++) {
+if(i%3 == 0 || i%5 == 0 || i%7 == 0 || i%9 == 0 || i%10 == 0) {
+sum+=i;
+} 
+}
+return sum;
+}
+}
